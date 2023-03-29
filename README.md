2b22cf4c4d6e9abca40aa19b15d6853b9bc810a3
Mario Ramos Montesinos <mariodarken@gmail.com>
2023-03-29 13:04:34 +0200

    WIP

--- README.md
+++ README.md
@@ -14,3 +14,18 @@ yarn && yarn start
 In a different terminal window for each app.
 
 The visit the [home page](http://localhost:3000/).
+
+# Explicacion
+
+Existen 4 microsfrontends:
+- Home
+    - MainLayout: con un header, footer y el contenido según la ruta
+    - HomeContent: la pagina principal, donde se muestran los productos
+- PDP
+    - PDPContent: la pagina de detalle de producto, donde se muestra el producto seleccionado
+- Cart
+    - CartContent: Es la pagina del carrito de compras, donde se muestran los productos seleccionados. 
+    - MiniCart: que se muestra en la parte superior derecha de la pagina
+    - Cart.js: contiene las funciones para agregar, obtener y eliminar productos del carrito. El carrito se almacena en el backend.
+- AddToCart
+    - AddToCart: Es el boton de login, que se muestra en la parte superior derecha de la pagina.
--- cart/src/cart.js
+++ cart/src/cart.js
@@ -1,13 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { BehaviorSubject } from "rxjs";
 
-const API_SERVER = "http://localhost:8080";
-
 export const jwt = new BehaviorSubject(null);
 export const cart = new BehaviorSubject(null);
 
 export const getCart = () =>
-  fetch(`${API_SERVER}/cart`, {
+  fetch(`${process.env.REACT_APP_API_SERVER}/cart`, {
     headers: {
       "Content-Type": "application/json",
       Authorization: `Bearer ${jwt.value}`,
@@ -20,7 +18,7 @@ export const getCart = () =>
     });
 
 export const addToCart = (id) =>
-  fetch(`${API_SERVER}/cart`, {
+  fetch(`${process.env.REACT_APP_API_SERVER}/cart`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -34,7 +32,7 @@ export const addToCart = (id) =>
     });
 
 export const clearCart = () =>
-  fetch(`${API_SERVER}/cart`, {
+  fetch(`${process.env.REACT_APP_API_SERVER}/cart`, {
     method: "DELETE",
     headers: {
       "Content-Type": "application/json",
@@ -47,7 +45,7 @@ export const clearCart = () =>
     });
 
 export const login = (username, password) =>
-  fetch(`${API_SERVER}/auth/login`, {
+  fetch(`${process.env.REACT_APP_API_SERVER}/auth/login`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
